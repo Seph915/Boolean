1cbbd605dcc668324112e419755056bdf7aaac7a
fabio <fabiocostanza91@gmail.com>
2023-08-03 11:03:28 +0200

    Exercise boolean

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
+//Try to solve on paper the following boolean algebra operations:
+
+        //[A]: 2 <= 2 && !true
+        //[B]: !false && 3 > 2
+        //[C]: considering that t=false and f=true: !(!t || f)
+        //[D]: 6 > 6 ^ !(true && true)
+        //Then compare your solutions with a Java program that tests the validity of your assumptions.
+
+
+
 public class Main {
     public static void main(String[] args) {
 
-        boolean resultA = 2 <= 2 && !true;
-        System.out.println("[A] Result: " + resultA);
+        boolean A = 2 <= 2 && !true;
+        boolean B = !false && 3 > 2;
+        boolean t = false; boolean f = true;
+        boolean C = !(!t || f);
+        boolean D = 6 > 6 ^ !(true && true);
 
+        System.out.println(B);
+        System.out.println(A);
+        System.out.println(C);
+        System.out.println(D);
 
-        boolean resultB = !false && 3 > 2;
-        System.out.println("[B] Result: " + resultB);
+            }
+        }
 
-
-        boolean t = false;
-        boolean f = true;
-        boolean resultC = !(t || f);
-        System.out.println("[C] Result: " + resultC);
-
-
-        boolean resultD = 6 > 6 ^ !(true && true);
-        System.out.println("[D] Result: " + resultD);
-    }
-}
